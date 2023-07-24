45d66746a14699a350174e01b64bcf0743225af3
Merten Dieckmann <merten.dieckmann@web.de>
2023-07-24 04:13:04 +0200

    fix: content can be void

--- src/components/editor/Engine.tsx
+++ src/components/editor/Engine.tsx
@@ -3,14 +3,14 @@ import { usePlayStore } from "@/stores/editor/PlayStore";
 
 export default function Engine() {
 
-    const [nodeContent, setNodeContent] = useState<React.ReactNode | null>(null);
+    const [nodeContent, setNodeContent] = useState<React.ReactNode | null | void>(null);
     const currentNode = usePlayStore((state) => state.currentNode);
     const isProcessRunning = usePlayStore(state => state.isProcessRunning)
 
     useEffect(() => {
         const runCurrentNode = async () => {
             if (currentNode !== null) {
-                const content = await currentNode.node.run() || null;
+                const content = await currentNode.node.run();
                 setNodeContent(content);
             }
         };
